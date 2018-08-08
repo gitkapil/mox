fda9cd7b4d2da6fccc8871e7a36dc7d9be52ee43
Prerna Pallavi <prerna.pallavi@industrieit.com>
2018-08-08 15:45:54 +1000

    minor code refactoring (removed implements BaseStep from helper files)

--- src/main/java/utils/DateHelper.java
+++ src/main/java/utils/DateHelper.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class DateHelper implements BaseStep{
+public class DateHelper{
 
     /**
      *
--- src/main/java/utils/General.java
+++ src/main/java/utils/General.java
@@ -3,7 +3,7 @@ package utils;
 
 import java.util.UUID;
 
-public class General implements BaseStep{
+public class General{
 
     /**
      *
