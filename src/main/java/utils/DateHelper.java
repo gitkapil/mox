43b553d5cbf9ce04eb3db52476687e15b1c023b2
Prerna Pallavi <prerna.pallavi@industrieit.com>
2018-08-08 15:38:32 +1000

    Added DateHelper class for dates related generic methods

---
+++ src/main/java/utils/DateHelper.java
@@ -0,0 +1,60 @@
+package utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DateHelper implements BaseStep{
+
+    /**
+     *
+     * @returns the current system date and timestamp
+     */
+    public Calendar getSystemDateandTimeStamp(){
+        Calendar currDateTime = Calendar.getInstance();
+        return currDateTime;
+    }
+
+    /**
+     *
+     * @param cal Calendar instance
+     * @param pattern format to whuch the date needs to get converted
+     * @returns the formatted string
+     */
+   public String convertDateTimeIntoAFormat(Calendar cal, String pattern){
+       SimpleDateFormat sdf= new SimpleDateFormat();
+        try{
+         sdf   = new SimpleDateFormat(pattern, Locale.US);
+       }
+
+       catch (Exception e){
+           return "";
+       }
+
+       return sdf.format(cal.getTime());
+   }
+
+
+    /**
+     *
+     * @param cal Calendar instance
+     * @param minutes to be subtracted
+     * @returns the new value
+     */
+    public Calendar subtractMinutesFromSystemDateTime(Calendar cal, int minutes){
+        try{
+
+            cal.add(Calendar.MINUTE, -minutes);
+        }
+
+        catch (Exception e){
+            return null;
+        }
+
+        return cal;
+    }
+
+
+
+
+}
