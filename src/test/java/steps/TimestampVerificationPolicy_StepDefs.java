e01d6990d7bdccf8b6445c01a47f305e14cca410
Prerna Pallavi <prerna.pallavi@industrieit.com>
2018-09-25 16:27:11 +1000

    - First commit for the story. Created a new feature file and step definition file - Removed "/" from the api_resource from playpen.properties

---
+++ src/test/java/steps/TimestampVerificationPolicy_StepDefs.java
@@ -0,0 +1,62 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import utils.BaseStep;
+
+import java.util.Calendar;
+
+
+public class TimestampVerificationPolicy_StepDefs implements BaseStep{
+    final static Logger logger = Logger.getLogger(TimestampVerificationPolicy_StepDefs.class);
+
+
+    @Given("^request date timestamp in the payment request header is less than (\\d+) mins than the current timestamp$")
+    public void request_date_timestamp_in_the_payment_request_header_is_less_than_mins_than_the_current_timestamp(int mins) {
+        Calendar timestamp= dateHelper.subtractMinutesFromSystemDateTime(dateHelper.getSystemDateandTimeStamp(), mins-1);
+
+        paymentRequest.setRequestDateTime(dateHelper.convertDateTimeIntoAFormat(timestamp, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+    }
+
+    @Given("^request date timestamp in the payment request header is exactly (\\d+) mins behind than the current timestamp$")
+    public void request_date_timestamp_in_the_payment_request_header_is_exactly_mins_behind_than_the_current_timestamp(int mins) {
+        Calendar timestamp= dateHelper.subtractMinutesFromSystemDateTime(dateHelper.getSystemDateandTimeStamp(), mins);
+
+        paymentRequest.setRequestDateTime(dateHelper.convertDateTimeIntoAFormat(timestamp, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+    }
+
+    @Given("^request date timestamp in the payment request header is more than (\\d+) mins than the current timestamp$")
+    public void request_date_timestamp_in_the_payment_request_header_is_more_than_mins_than_the_current_timestamp(int mins) {
+        Calendar timestamp= dateHelper.subtractMinutesFromSystemDateTime(dateHelper.getSystemDateandTimeStamp(), mins+1);
+
+        paymentRequest.setRequestDateTime(dateHelper.convertDateTimeIntoAFormat(timestamp, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+    }
+
+    @Given("^request date timestamp in the payment status header is less than (\\d+) mins than the current timestamp$")
+    public void request_date_timestamp_in_the_payment_rstatus_header_is_less_than_mins_than_the_current_timestamp(int mins) {
+        Calendar timestamp= dateHelper.subtractMinutesFromSystemDateTime(dateHelper.getSystemDateandTimeStamp(), mins-1);
+
+       // paymentStatus.setRequestDateTime(dateHelper.convertDateTimeIntoAFormat(timestamp, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+    }
+
+    @Given("^request date timestamp in the payment status header is exactly (\\d+) mins behind than the current timestamp$")
+    public void request_date_timestamp_in_the_payment_rstatus_header_is_exactly_mins_behind_than_the_current_timestamp(int mins) {
+        Calendar timestamp= dateHelper.subtractMinutesFromSystemDateTime(dateHelper.getSystemDateandTimeStamp(), mins);
+
+       // paymentStatus.setRequestDateTime(dateHelper.convertDateTimeIntoAFormat(timestamp, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+    }
+
+    @Given("^request date timestamp in the payment status header is more than (\\d+) mins than the current timestamp$")
+    public void request_date_timestamp_in_the_payment_status_header_is_more_than_mins_than_the_current_timestamp(int mins) {
+        Calendar timestamp= dateHelper.subtractMinutesFromSystemDateTime(dateHelper.getSystemDateandTimeStamp(), mins+1);
+
+       // paymentStatus.setRequestDateTime(dateHelper.convertDateTimeIntoAFormat(timestamp, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+    }
+
+}
+
+
+
---
+++ src/test/resources/features/TimestampVerificationPolicy.feature
@@ -0,0 +1,68 @@
+Feature: Timestamp Verification Policy - DRAG- 577
+
+Background: Retrieving access Token
+Given I am an user
+When I make a request to the Dragon ID Manager
+Then I recieve an access_token
+
+@regression
+Scenario: Positive flow- (System time) < (Request-Date-Time + Tolerance) within payment request
+  Given I am an authorized user
+  And I have valid payment details
+  And request date timestamp in the payment request header is less than 5 mins than the current timestamp
+  And I make a request for the payment
+  And I should recieve a successful payment response
+
+@regression
+Scenario: Positive flow- (System time) = (Request-Date-Time + Tolerance) within payment request
+  Given I am an authorized user
+  And I have valid payment details
+  And request date timestamp in the payment request header is exactly 5 mins behind than the current timestamp
+  And I make a request for the payment
+  And I should recieve a successful payment response
+
+
+@regression
+Scenario: Negative flow- (System time) > (Request-Date-Time + Tolerance) within payment request
+  Given I am an authorized user
+  And I have valid payment details
+  And request date timestamp in the payment request header is more than 5 mins than the current timestamp
+  And I make a request for the payment
+  Then I should recieve a "400" error response with "Request timestamp too old" error description and "EA002" errorcode within payment response
+  And error message should be "Service Request Validation Failed" within payment response
+
+
+@regression
+Scenario: Positive flow- (System time) < (Request-Date-Time + Tolerance) within payment status request
+  Given I am an authorized user
+  And I have valid payment details
+  And I make a request for the payment
+  And I should recieve a successful payment response
+  And I have a valid payment id
+  And request date timestamp in the payment status header is less than 5 mins than the current timestamp
+  When I make a request for the check status
+  Then I should recieve a successful check status response
+
+
+@regression
+Scenario: Positive flow- (System time) = (Request-Date-Time + Tolerance) within payment status request
+  Given I am an authorized user
+  And I have valid payment details
+  And I make a request for the payment
+  And I should recieve a successful payment response
+  And I have a valid payment id
+  And request date timestamp in the payment status header is exactly 5 mins behind than the current timestamp
+  When I make a request for the check status
+  Then I should recieve a successful check status response
+
+
+@regression
+Scenario: Positive flow- (System time) > (Request-Date-Time + Tolerance) within payment status request
+  Given I am an authorized user
+  And I have valid payment details
+  And I make a request for the payment
+  And I should recieve a successful payment response
+  And I have a valid payment id
+  And request date timestamp in the payment status header is more than 5 mins than the current timestamp
+  When I make a request for the check status
+  Then I should recieve a successful check status response
