921b0c74771b2a2693d5ca60bad88db033292de0
Prerna Pallavi <prerna.pallavi@industrieit.com>
2018-09-28 09:35:15 +1000

    - removed unused imports

--- src/test/java/steps/TimestampVerificationPolicy_StepDefs.java
+++ src/test/java/steps/TimestampVerificationPolicy_StepDefs.java
@@ -1,10 +1,7 @@
 package steps;
 
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 import org.apache.log4j.Logger;
-import org.junit.Assert;
 import utils.BaseStep;
 
 import java.util.Calendar;
