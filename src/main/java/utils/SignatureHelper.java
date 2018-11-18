0cc10863950da9af12f55670a28303666cc453f3
Prerna Pallavi <prerna.pallavi@industrieit.com>
2018-11-19 08:39:29 +1100

    removed an unused throwable

--- src/main/java/utils/SignatureHelper.java
+++ src/main/java/utils/SignatureHelper.java
@@ -61,7 +61,7 @@ public class SignatureHelper {
         }
     }
 
-    public String calculateContentDigestHeader(byte[] content) throws NoSuchAlgorithmException {
+    public String calculateContentDigestHeader(byte[] content){
         //final byte[] digest = MessageDigest.getInstance("SHA-256").digest(content);
         final String digest = DigestUtils.sha256Hex(content);
         final String digestHeader = "SHA-256=" + new String(Base64.encodeBase64(digest.getBytes()));
