4a6c932235001f2c75203c2d62fcbaf3406b8638
Prerna Pallavi <prerna.pallavi@industrieit.com>
2018-11-30 15:54:24 +1100

    - removed unused imports from signatureHelper.java

--- src/main/java/utils/SignatureHelper.java
+++ src/main/java/utils/SignatureHelper.java
@@ -2,7 +2,6 @@ package utils;
 
 import com.jayway.restassured.response.Header;
 import com.jayway.restassured.response.Response;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
