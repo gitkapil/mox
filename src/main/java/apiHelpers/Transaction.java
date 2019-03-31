7cb6dab38d5abf39ff03cb69f4aa51bcc38029a5
Wilson Chan <wilson.chan@industrieit.com>
2019-03-31 12:48:12 +0800

    Working with new policy file update

--- src/main/java/apiHelpers/Transaction.java
+++ src/main/java/apiHelpers/Transaction.java
@@ -1,7 +1,5 @@
 package apiHelpers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.restassured.response.Response;
 import managers.UtilManager;
 import org.junit.Assert;
@@ -9,6 +7,7 @@ import utils.EnvHelper;
 import utils.PropertyHelper;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -101,7 +100,7 @@ public class Transaction extends UtilManager {
         try {
             transactionListResponse = getRestHelper().getRequestWithHeadersAndQueryStringParams(url, returnTransactionListHeader(
                     "GET",
-                    "/transactions",
+                    new URL(url).getPath(),
                     signingKeyId,
                     signingAlgorithm,
                     signingKey,
