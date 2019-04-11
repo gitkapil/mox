2278bb7fcdad0c0816598173e94787c4bda2537d
Johnny Tam <jtam1980@hotmail.com>
2019-04-11 10:43:47 +0800

    DRAG-1440 Write java steps

---
+++ src/test/java/steps/ManagementGetApplications_StepDefs.java
@@ -0,0 +1,69 @@
+package steps;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import managers.TestContext;
+import managers.UtilManager;
+import org.apache.log4j.Logger;
+
+public class ManagementGetApplications_StepDefs extends UtilManager {
+    TestContext testContext;
+
+    public ManagementGetApplications_StepDefs(TestContext testContext) {
+        this.testContext = testContext;
+    }
+
+    final static Logger logger = Logger.getLogger(ManagementGetApplications_StepDefs.class);
+
+    @And("" +
+            "^I then clean and create 30 test applications$")
+    public void clean_and_create_applications() {
+
+    }
+
+    @When("^I get a list of applications without any filters$")
+    public void list_of_applications_without_any_filters() {
+
+    }
+
+    @Then("^I should receive a successful response$")
+    public void successful_response() {
+
+    }
+
+    @And("^the response should have a list of ([^\"]*) applications$")
+    public void should_have_number_of_applications(int numberOfResponses) {
+
+    }
+
+    @And("^the response should have more than ([^\"]*) in total$")
+    public void should_have_more_than_responses_in_total(int numberOfResponses) {
+
+    }
+
+    @When("^I get a list of applications using filters to filter \"([^\"]*)\" with \"([^\"]*)\"$")
+    public void get_a_list_of_applications_using_filters(String filterName, String filterValue) {
+
+    }
+
+    @When("^I get a list of applications using filters to filter \"([^\"]*)\" with \"([^\"]*)\" with ([^\"]*) limits$")
+    public void get_a_list_of_apps_using_filters_and_limits(String filterName, String filerValue, int limit) {
+
+    }
+
+    @And("^the response should have a ([^\"]*) number of total pages$")
+    public void response_should_have_number_of_total_pages(int totalNumberOfPages) {
+
+    }
+
+    @And("^the response should be on page ([^\"]*)$")
+    public void response_should_be_on_page(int currentPageNumber) {
+
+    }
+
+    @When("^I move to page ([^\"]*)$")
+    public void move_tot_page(int nextPageNumber) {
+
+    }
+}
--- src/test/resources/features/ManagementGetApplications.feature
+++ src/test/resources/features/ManagementGetApplications.feature
@@ -1,21 +1,25 @@
 Feature: Merchant Management API - GET applications
 
   Background: Retrieving access Token
-    Given I am an user
+    Given I am an authorized CSO user
     When I make a request to the Dragon ID Manager
     Then I receive an access_token
     And I then clean and create 30 test applications
 
   @regression @merchantManagement @merchantManagementGet
-  Scenario: Positive flow - Get a list of applications
+  Scenario Outline: Positive flow - Get a list of applications
     Given I am an authorized CSO user
     When I get a list of applications without any filters
     Then I should receive a successful response
-    And the response should have a list of 20 applications
-    And the response should have more than 20 applications in total
+    And the response should have a list of <numberOfResponses> applications
+    And the response should have more than <numberOfResponses> in total
+
+    Examples:
+    |numberOfResponses|
+    |20               |
 
   @regression @merchantManagement @merchantManagementGet
-  Scenario Outline: Postivie flow - Get a list of application using filters
+  Scenario Outline: Positive flow - Get a list of application using filters
     Given I am an authorized CSO user
     When I get a list of applications using filters to filter "<filterName>" with "<filterValue>"
     Then I should receive a successful response
