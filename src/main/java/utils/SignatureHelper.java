655a380070d6bc52d0e5b320ea4eaba57fddb1de
Johnny Tam <jtam1980@hotmail.com>
2019-05-30 22:50:23 +0800

    Update regression tests

--- src/main/java/utils/SignatureHelper.java
+++ src/main/java/utils/SignatureHelper.java
@@ -28,7 +28,7 @@ public class SignatureHelper {
         final Signer signer =
                 new Signer(key, new Signature(keyId, algo, null, obtainSignatureHeaders(presentHeaders.keySet(), includeHeaders)));
         Signature signed = signer.sign(method, url, presentHeaders);
-        System.out.println("JT: " + url);
+
         return signed.toString().replace("Signature ", "");
     }
 
