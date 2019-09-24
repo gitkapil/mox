cca4fd940779a52758029d5bf7699eb8c7098e54
kapil.c.kumar <kapil.c.kumar@capgemini.com>
2019-09-24 14:40:38 +0800

    added PUT platform API code

--- src/main/java/apiHelpers/PutPlatform.java
+++ src/main/java/apiHelpers/PutPlatform.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.restassured.response.Response;
 import managers.UtilManager;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import utils.EnvHelper;
@@ -18,6 +19,9 @@ public class PutPlatform extends UtilManager {
     private String platformStatus;
 
 
+    private String platformId;
+
+
     public String getAuthToken() {
         return authToken;
     }
@@ -30,16 +34,8 @@ public class PutPlatform extends UtilManager {
         return platformName;
     }
 
-    public void setPlatformName(String platformName) {
-        this.platformName = platformName;
-    }
-
     public String getPlatformStatus() {
         return platformStatus;
-    }
-
-    public void setPlatformStatus(String platformStatus) {
-        this.platformStatus = platformStatus;
     }
 
     public HashMap<String, String> getRequestHeader() {
@@ -62,8 +58,15 @@ public class PutPlatform extends UtilManager {
         return platformDescription;
     }
 
-    public void setPlatformNameDescription(String description)
-    {
+    public String getPlatformId() {
+        return platformId;
+    }
+
+    public void setPlatformId(String platformId) {
+        this.platformId = platformId;
+    }
+
+    public void setPlatformNameDescription(String description) {
         if (description.equalsIgnoreCase("bigDescription")) {
             this.platformDescription = StringUtils.repeat("*", 300);
         } else {
@@ -83,30 +86,92 @@ public class PutPlatform extends UtilManager {
         this.response = response;
     }
 
-    private void returnRequestBody() {
+    private HashMap<String, String> returnRequestBody() {
         requestBody.clear();
+        requestBody.put("description", getPlatformDescription());
+        requestBody.put("platformName", getPlatformName());
+        requestBody.put("status", getPlatformStatus());
+        return requestBody;
+    }
 
-        if (platformDescription != null) {
-            requestBody.put("platformDescription", platformDescription);
+    private HashMap<String, String> returnRequestBodyWithOneField(String onlyInputBodyField) {
+        requestBody.clear();
+        if (onlyInputBodyField.equalsIgnoreCase("description")) {
+            requestBody.put("description", getPlatformDescription());
+        } else if (onlyInputBodyField.equalsIgnoreCase("status")) {
+            requestBody.put("status", getPlatformStatus());
+        } else {
+            requestBody.put("platformName", getPlatformName());
         }
-        if (platformName != null) {
-            requestBody.put("platformName", platformName);
-        }
-        if (platformStatus != null) {
-            requestBody.put("status", platformStatus);
+
+        return requestBody;
+    }
+
+    private HashMap<String, String> returnRequestBodyWithMissingOptionalFields(String missingBody) {
+        requestBody.clear();
+        requestBody.put("description", getPlatformDescription());
+        requestBody.put("platformName", getPlatformName());
+        requestBody.put("status", getPlatformStatus());
+        requestBody.remove(missingBody);
+        return requestBody;
+    }
+
+    public void setPlatformName(String platformName) {
+        if (platformName.equalsIgnoreCase("longName")) {
+            this.platformName = StringUtils.repeat("*", 300);
+        } else if (platformName.equalsIgnoreCase("validName")) {
+            this.platformName = RandomStringUtils.randomAlphabetic(10);
+            ;
+        } else if (platformName.equalsIgnoreCase("onlySpecialCharacter")) {
+            this.platformName = RandomStringUtils.random(10, 33, 47, false, false);
+            ;
+        } else if (platformName.equalsIgnoreCase("existingPlatformName")) {
+            this.platformName = "DRAGON PLATFORM";
+        } else if (platformName.equalsIgnoreCase("space")) {
+            this.platformName = " ";
+        } else if (platformName.equalsIgnoreCase("emptySpace")) {
+            this.platformName = "";
+        } else {
+            this.platformName = platformName;
         }
     }
 
-    private void returnRequestHeader() {
-        requestHeader.clear();
+    public void setPlatformDescription(String platformDescription) {
+        if (platformDescription.equalsIgnoreCase("validDescription")) {
+            this.platformDescription = RandomStringUtils.randomAlphabetic(20);
+        } else if (platformDescription.equalsIgnoreCase("longDescription")) {
+            this.platformDescription = StringUtils.repeat("*", 300);
+        } else if (platformDescription.equalsIgnoreCase("space")) {
+            this.platformDescription = " ";
+        } else if (platformDescription.equalsIgnoreCase("emptySpace")) {
+            this.platformDescription = "";
+        } else {
+            this.platformDescription = platformDescription;
+        }
+    }
 
+    public void setPlatformStatus(String platformStatus) {
+        if (platformStatus.equalsIgnoreCase("active")) {
+            this.platformStatus = "A";
+        } else if (platformStatus.equalsIgnoreCase("deactivated")) {
+            this.platformStatus = "D";
+        } else if (platformStatus.equalsIgnoreCase("invalidStatus")) {
+            this.platformStatus = "P";
+        } else if (platformStatus.equalsIgnoreCase("space")) {
+            this.platformStatus = " ";
+        } else if (platformStatus.equalsIgnoreCase("emptySpace")) {
+            this.platformStatus = "";
+        }
+    }
+
+    private HashMap<String, String> returnRequestHeader() {
+        requestHeader.clear();
         requestHeader.put("Accept", "application/json");
         requestHeader.put("Content-Type", "application/json");
         requestHeader.put("Authorization", authToken);
         requestHeader.put("Trace-Id", getGeneral().generateUniqueUUID());
         requestHeader.put("Request-Date-Time", getDateHelper().getUTCNowDateTime());
         requestHeader.put("Api-Version", PropertyHelper.getInstance().getPropertyCascading("version"));
-
         if (EnvHelper.getInstance().isLocalDevMode()) {
             EnvHelper.getInstance().addMissingHeaderForLocalDevMode(requestHeader);
         }
@@ -117,15 +182,63 @@ public class PutPlatform extends UtilManager {
             e.printStackTrace();
             Assert.assertTrue("Trouble creating Digest!", false);
         }
+        return requestHeader;
+    }
+
+    private HashMap<String, String> returnRequestHeaderWithMissingKey(String keys) {
+        requestHeader.clear();
+        requestHeader.put("Accept", "application/json");
+        requestHeader.put("Content-Type", "application/json");
+        requestHeader.put("Authorization", authToken);
+        requestHeader.put("Trace-Id", getGeneral().generateUniqueUUID());
+        requestHeader.put("Request-Date-Time", getDateHelper().getUTCNowDateTime());
+        requestHeader.put("Api-Version", PropertyHelper.getInstance().getPropertyCascading("version"));
+        if (EnvHelper.getInstance().isLocalDevMode()) {
+            EnvHelper.getInstance().addMissingHeaderForLocalDevMode(requestHeader);
+        }
+        try {
+            requestHeader.put("Digest", getSignatureHelper().calculateContentDigestHeader(
+                    new ObjectMapper().writeValueAsBytes(requestBody)));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            Assert.assertTrue("Trouble creating Digest!", false);
+        }
+        requestHeader.remove(keys);
+        return requestHeader;
     }
 
     public void makeRequest(String url) {
-        returnRequestBody();
-        returnRequestHeader();
+
         response = getRestHelper().putRequestWithHeaderAndBody(url,
-                requestHeader, requestBody);
-        System.out.println("response after make request: " + response.getBody().asString());
+                returnRequestHeader(), returnRequestBody());
+        System.out.println("response after make request: " + response.getBody().prettyPrint());
     }
 
+    public void makeRequest(String url, String missingBody) {
 
+        response = getRestHelper().putRequestWithHeaderAndBody(url,
+                returnRequestHeader(), returnRequestBodyWithMissingOptionalFields(missingBody));
+        System.out.println("response after make request: " + response.getBody().prettyPrint());
+    }
+
+    public void makeRequestWithMissingHeader(String url, String keys) {
+
+        response = getRestHelper().putRequestWithHeaderAndBody(url,
+                returnRequestHeaderWithMissingKey(keys), returnRequestBody());
+        System.out.println("response after make request: " + response.getBody().prettyPrint());
+    }
+
+    public void makeRequestWithOneFiled(String url, String onlyBodyField) {
+
+        response = getRestHelper().putRequestWithHeaderAndBody(url,
+                returnRequestHeader(), returnRequestBodyWithOneField(onlyBodyField));
+        System.out.println("response after make request: " + response.getBody().prettyPrint());
+    }
+
+    public void setAuthTokenWithBearer(String authToken) {
+        this.authToken = "Bearer " + authToken;
+    }
+    public void setAuthTokenWithoutBearer(String authToken) {
+        this.authToken =authToken;
+    }
 }
